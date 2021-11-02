fe74ff8c004259d92836a48cd65a65890381ac66
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-11-01 19:47:45 -0500

    Update marca.py

--- televisores/marca.py
+++ televisores/marca.py
@@ -4,7 +4,7 @@ class Marca:
     def __init__(self, nombre):
         self.__nombre = nombre
 
-    def __int__(self):
+    def getNombre(self):
         return self.__nombre
 
     def setNombre(self, nombre):
